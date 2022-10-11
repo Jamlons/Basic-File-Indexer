ab1391e533d9c0b1496303dfda38f4a1f2d732bd
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-11 13:07:16 +0800

    working on indexing

--- indexing.c
+++ indexing.c
@@ -1,1 +1,16 @@
 #include "trove.h"
+#include  <stdio.h>
+#include  <sys/types.h>
+#include  <sys/stat.h>
+#include  <sys/param.h>
+#include  <dirent.h>
+#include  <unistd.h>
+
+void create_file(char *filename) {
+  FILE *fp;
+  fp = fopen(filename, "w");
+  if (fp == NULL) {
+    perror (progname);
+    exit(EXIT_FAILURE);
+  }
+}

3e872022f3b4db065acb903d43963554890988fb
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-11 11:03:48 +0800

    Added the ability to determine if is a dir or file

--- findfiles.c
+++ findfiles.c
@@ -1,1 +1,22 @@
 #include "trove.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+void file_attributes(char *filename) {
+  struct stat stat_buffer;
+  
+  if(stat(filename, &stat_buffer) != 0) {
+    perror(progname);
+    exit(EXIT_FAILURE);
+  }
+  else if (S_ISREG(stat_buffer.st_mode)) {
+    // Is a file - is readable 
+  }
+  else if (S_ISDIR(stat_buffer.st_mode)) {
+    // Is a directory - is readable 
+  }
+}

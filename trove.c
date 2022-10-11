29824da4f8a40bb1d8470241a4d0ff976296a2dc
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-11 15:00:16 +0800

    Fixed a bug

--- trove.c
+++ trove.c
@@ -50,7 +50,7 @@ void main(int argc, char *argv[]) {
             word_length = atoi(optarg);
         }
         // If no commands are given, this is the word.
-        else if (bflag && rflag && uflag) {
+        else if (bflag || rflag || uflag) {
             fs->word = strdup(optarg);
         }
         // If one command is given, this is the filelist

961a7b833b7cd7f5b27107d08d326cbd3c3feb0d
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-11 15:26:03 +0800

    Added ability to read through a given directory

--- findfiles.c
+++ findfiles.c
@@ -6,17 +6,43 @@
 #include <time.h>
 #include <unistd.h>
 
-void file_attributes(char *filename) {
+int file_attributes(char *filename) {
   struct stat stat_buffer;
   
   if(stat(filename, &stat_buffer) != 0) {
     perror(progname);
     exit(EXIT_FAILURE);
   }
+  // Is a file - is readable 
   else if (S_ISREG(stat_buffer.st_mode)) {
-    // Is a file - is readable 
+    return 1;
   }
+  // Is a directory - is readable 
   else if (S_ISDIR(stat_buffer.st_mode)) {
-    // Is a directory - is readable 
+    return 2;
   }
+}
+
+void real_file(char *filename) {
+  
+}
+
+void list_directory(char *dirname) {
+  DIR *dirp;
+  struct dirent *dp;
+  
+  dirp = opendir(dirname);
+  if (dirp == NULL) {
+    perror( progname );
+    exit(EXIT_FAILURE);
+  }
+  while((dp = readdir(dirp)) != NULL) {
+     int file_type = file_attributes(dp->d_name);
+     if (file_type = 1) {
+       printf("\n%s\n", dp->d_name);
+     }
+     else
+       printf("\n%s\n", "File is a directory);
+  }
+  closedir(dirp);
 }

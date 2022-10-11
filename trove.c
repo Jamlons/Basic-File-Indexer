a9833230a312c2886616b79747b90d4a316e53ec
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-11 13:37:20 +0800

    Added getopt arguments

--- trove.c
+++ trove.c
@@ -1,5 +1,39 @@
 #include "trove.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
 
-main(int argc, char* argv[]) {
+char *progname;
+#define OPTLIST "f:brul:"
+
+main(int argc, char *argv[]) {
+    struct file_storage *fs;
+    int opt;
+    char *filename = NULL;
+    int value = DEFAULT_VALUE;
+    fs->file_name = strdup(default_file_name);
     
+    opterr = 0;
+    while ((opt = getopt(argc, argv, OPTLIST)) != -1) {
+        if (opt == 'f') {
+            fs->file_name = strdup(optarg);
+        }
+        else if (opt == 'b') {
+            create_file(fs->file_name);
+            // Add info from file list to created file
+        }
+        else if (opt == 'r') {
+            //Remove file info from trove_file
+        }
+        else if (opt == 'u') {
+            // Update info from trove_file
+        }
+        else if (opt == 'l') {
+            // Grab word length
+        }
+    }
+    for (; optind < argc;
 }

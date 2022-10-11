e2d08fe1589153d6aa4cfb052ed4ed9cae48f695
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-11 09:48:40 +0800

    Updated trove.h

--- trove.h
+++ trove.h
@@ -1,5 +1,13 @@
+#ifndef TROVE_H
+#define TROVE_H
+
+extern void file_attributes(char *filename);
+extern void read_file(char *filename);
+
+extern int max_word_length;
 
 
-typedef struct {
-    
-}names;
+struct file_storage {
+    char *file_name;
+    char *word;
+};

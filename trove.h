407eb19b2c9cf3b4f7edef4767b2d46e2decf9fb
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-12 12:02:21 +0800

    Added read and open trove file

--- trove.h
+++ trove.h
@@ -6,6 +6,8 @@ extern char *default_file_name;
 extern void file_attributes(char *filename);
 extern void read_file(char *filename);
 extern void create_trove(char *trovename);
+extern FILE *open_trove(char *filename);
+extern FILE *read_trove(char *filename);
 
 extern int min_word_length;
 

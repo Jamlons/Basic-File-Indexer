beaf61c27fb3ac05c3f8a17bb3a7002edb2cb4eb
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-12 13:31:26 +0800

    Starting to add removal

--- remove.c
+++ remove.c
@@ -1,2 +1,22 @@
 //FUNCTION TO REMOVE FILE INFO FROM GIVEN TROVEFILE
 #include "trove.h"
+
+//To remove lines from files - must duplicate the text file without said lines in it
+void remove_files(FILE *fp) {
+  // If provided file isn't valid
+  if (fp == NULL) {
+   printf("Unable to open file given");
+    perror(progname);
+    exit(EXIT_FAILURE);
+  }
+  FILE *new_file = create_trove('tempfile.txt');
+  // If unable to open tempfile
+  if (new_file == NULL) {
+    printf("Unable to open temp file, may not have permission");
+    perror(progname);
+    exit(EXIT_FAILURE);
+  }
+  while (!feof(fp)) {
+  
+  }
+}

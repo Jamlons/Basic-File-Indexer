ecd194fcbf446200b48772696bfad2fadb42eb3f
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-13 13:24:06 +0800

    Update filenames.c

--- filenames.c
+++ filenames.c
@@ -2,5 +2,26 @@
 #include "trove.h"
 
 void find_names(FILE *fp) {
+  char *buf;
+  char *line = NULL;
+  char *file_path;
+  size_t linesize = 0;
+  ssize_t linelen;
+  FILE *fp = fopen(filename, "r");
+  FILE *trovefile = append_trove(fs->file_name);
+  // fopen failed
+  if (fp == NULL) {
+    perror(progname);
+    exit(EXIT_FAILURE);
+  }
   
+  while ((linelen = getline(&line, &linesize, fp)) != -1) {
+    buf = strtok(line, ' ');
+    file_path = buf;
+    while (buf != NULL) {
+      buf = strtok(NULL, ' ');
+      if(buf == 
+    }
+      
+  }
 }

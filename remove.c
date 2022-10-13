e07131d3d04afde5b1b9c1a5b4516dc6b2bffdcb
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-13 14:58:01 +0800

    Update remove.c

--- remove.c
+++ remove.c
@@ -2,23 +2,17 @@
 #include "trove.h"
 
 //To remove lines from files - must duplicate the text file without said lines in it
-void remove_files(FILE *fp) {
-  // If provided file isn't valid
+void remove_files(FILE *fp, char *remove_file_name) {
+  READ_FILE_STORAGE read_file_storage;
+  FILE *new_file = create_trove('This_isAnObsurWeName_H90peuFully_en0uGh.txt');
   char *buf;
-  char *bufsizing;
-  if (fp == NULL) {
-   printf("Unable to open file given");
-    perror(progname);
-    exit(EXIT_FAILURE);
-  }
-  FILE *new_file = create_trove('tempfile.txt');
-  // If unable to open tempfile
-  if (new_file == NULL) {
-    printf("Unable to open temp file, may not have permission");
-    perror(progname);
-    exit(EXIT_FAILURE);
-  }
-  while (fgets(bufsizing, sizeof bufsizing, fp) != NULL) {
+  char *line = NULL;
+  char *file_path;
+  size_t linesize = 0;
+  ssize_t linelen;
+  while ((linelen = getline(&line, &linesize, fp)) != -1) {
+    buf = strtok(line, ' ');
     
   }
+  close (line);
 }

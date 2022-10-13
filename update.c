dc277afa60d4327f9a422885f792dcbc2ab519ca
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-13 15:53:53 +0800

    Added build_trove

--- update.c
+++ update.c
@@ -1,5 +1,6 @@
 //FUNCTION FOR UPDATING GIVEN FILES INSIDE GIVEN TROVEFILE
 #include "trove.h"
+#include "file_edits.h"
 
 void update_trove(char *file_name, int counter) {
   FILE *update_file_pointer = read_trove(file_name);
@@ -12,4 +13,24 @@ void update_trove(char *file_name, int counter) {
   for (int y = 0; y < counter; y ++) {
     build_trove(update_file_pointer); 
   }
+}
+
+void build_trove(char *filename) {
+    FILE *build_pointer = create_trove(filename);
+    if (build_pointer == NULL) {
+      printf("Unable to open %s, please make sure it is correct", filename);
+      perror(progname);
+      exit(EXIT_FAILURE);
+    }
+    int file_type = file_attributes(filelist[y]);
+    // file is directory
+    if (file_type == 1) {
+        list_directory(filelist[y]);
+    }
+    // file is reg file
+    else if (file_type == 2) {
+        add_file_path(build_pointer, filelist[y]);
+        read_file(filelist[y]);
+    }
+  fclose(build_pointer);
 }

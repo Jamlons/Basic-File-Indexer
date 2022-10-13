637d8be47833c703b2abe9eb7da4ebfe2441eae5
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-13 09:39:07 +0800

    Update trove.c

--- trove.c
+++ trove.c
@@ -77,11 +77,11 @@ int main(int argc, char *argv[]) {
             int file_type = file_attributes(argv[optind]);
             // file is directory
             if (file_type == 1) {
-                add_file_path(&new_file_pointer, argv[optind]);
                 list_directory(argv[optind]);
             }
             // file is reg file
             else if (file_type == 2) {
+                add_file_path(%new_file_pointer, argv[optind]);
                 read_file(argv[optind]);
             }
         }

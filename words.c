17409abc3a82ec54693b6e288c8d060d740ae5c3
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-13 09:32:52 +0800

    Finished read_file
    Should read and add the words found within the file_path and add them to the given trove file

--- words.c
+++ words.c
@@ -3,23 +3,26 @@
 
 void read_file(char *filename) {
   char *buf;
-  FILE *fp;
-  FILE *trovefile;
   char *line; 
-  fp = fopen(filename, "r");
+  FILE *fp = fopen(filename, "r");
+  FILE *trovefile = append_trove(fs->file_name);
   // fopen failed
-  if (fp == NULL){
-   perror(progname);
+  if (fp == NULL) {
+    perror(progname);
     exit(EXIT_FAILURE);
   }
-  
-  trovefile = append_trove(
+  // if append_trove failed
+  if (trovefile == NULL) {
+    perror(progname);
+    exit(EXIT_FAILURE);
+  }
   // While there are lines within the file
   while (fgets(line, sizeof line, fp) != NULL {
     // Make each non-alpha character a space
     for (char *temp = line; *temp; temp++) {
-      if (!isalpha(*temp))
+      if (!isalpha(*temp)) {
         *temp = ' ';
+      }
     }
     // Grab the alphanumeric word
     buf = strtok(line, ' ');
@@ -27,10 +30,13 @@ void read_file(char *filename) {
     while (buf != NULL) {
       // If buffered word is bigger than min length
       if (strlen(buf) >= min_word_length) {
-      // this is a word that can now be saved for index.
+        // Add word to buffer
+        fprintf(trovefile, "%s ", buf);
       }
       // Grab next tokenised word
       buf = strtok(NULL, ' ');
     }
   }
+  fclose(fp);
+  fclose(trovefile);
 }

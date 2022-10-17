c4b4b7de55efeeed05742b0cf6dc155c71af29a4
Joel <jamlons@users.noreply.github.com>
2022-10-17 12:15:41 +0800

    Got the code compiling now

--- filenames.c
+++ filenames.c
@@ -1,26 +1,32 @@
 //FUNCTION FOR FINDING THE GIVEN WORD WITHIN THE GIVEN TROVEFILE
 #include "trove.h"
+#include <string.h>
+#include <stdio.h>
 
-int find_names(FILE *fp) {
-  READ_FILE_STORAGE read_file_storage;
+int find_names(FILE *fp, char *word) {
   int amount_of_files_found = 0;
   char *buf;
   char *line = NULL;
   char *file_path;
   size_t linesize = 0;
   ssize_t linelen;
+  // While grabbing each line from the file
   while ((linelen = getline(&line, &linesize, fp)) != -1) {
-    buf = strtok(line, ' ');
+    // Parse the line with a space token - first one is the filepath 
+    //(this wont work if the filepath includes spaces)
+    buf = strtok(line, " ");
     file_path = buf;
     while (buf != NULL) {
-      buf = strtok(NULL, ' ');
-      if(!strcmp(read_file_storage.word, buf) {
+      // While parsing through each word.
+      buf = strtok(NULL, " ");
+      // If the parsed word does equal the word we are searching for
+      if(!strcmp(word, buf)) {
+        // Print the word out
         printf("%s\n", file_path);
         amount_of_files_found++;
         break;
       }
     }  
   }
-  close (line);
   return amount_of_files_found;
 }

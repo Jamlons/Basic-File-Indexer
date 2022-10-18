72a798069f12250c27588d81396d519536fe308a
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-18 10:20:52 +0800

    Added remove functionality - please test if it works.
    Added additional comments

--- remove.c
+++ remove.c
@@ -12,11 +12,13 @@ void remove_files(char *old_file_name, int counter, char *to_be_removed_file) {
   char *temp_file_name = "as239gnma9yhq9109k23.txt";
   FILE *new_file = create_trove(temp_file_name);
   FILE *old_file = read_trove(old_file_name);
+  // If file stream failed
   if (new_file == NULL) {
     printf("Unable to open %s, weird...", temp_file_name);
     perror(progname);
     exit(EXIT_FAILURE);
   }
+  // If file stream failed
   if (old_file == NULL) {
     printf("Unable to open %s, please make sure it is correct", old_file_name);
     perror(progname);
@@ -28,22 +30,34 @@ void remove_files(char *old_file_name, int counter, char *to_be_removed_file) {
   size_t linesize = 0;
   ssize_t linelen;
   int count = 1;
+  // While grabbing each line of the text file
   while ((linelen = getline(&line, &linesize, old_file)) != -1) {
+    // If line is a file path (odd number)
     if ((count % 2) != 0) {
+      // Save file path to a temp location
       file_path = strdup(line);
+      // For every file in filelist
       for (int a = 0; a < counter; a++) {
+        // If the file path and file list equal
         if (!strcmp(file_path, filelist[a]) {
+          // Increase count by two
+          // This will skip over both the file path and the words in said file
           count = count + 2;
           continue;
         }
       }
+      // If the file path is not to be removed add it to the new file
       fprintf(new_file, "%s\n", file_path);
       count++;
+      continue;
     }
+    // File path must be true - add all words to next line
     else {
       fprintf(new_file, "%s\n", line);
     }
+  // Remove old file
   remove(to_be_removed_file);
+  // Rename new file as old file name
   rename(temp_file_name, to_be_removed_file);
   fclose(new_file);
   fclose(old_file);

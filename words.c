803b5d594fa6804b8c64f712c851d8e873adebfd
Jamlons <105296543+Jamlons@users.noreply.github.com>
2022-10-20 08:55:04 +0800

    Added the ability to only save words required

--- words.c
+++ words.c
@@ -4,11 +4,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+
+typedef struct {
+  int wordcounter;
+  char * wordlist[];
+}WORD_STRUCTURE;
+
+WORD_STRUCTURE ws;
 
 // Function for adding all words of a given file of a given length to a given file
 void read_file(FILE *build_pointer, char *filename) {
   // Global structure
   READ_FILE_STRUCTURE *rfs = &read_file_structure;
+  ws.wordcounter = 0;
+  int flag = 0;
   char *buf;
   char *line = NULL;
   size_t linesize = 0;
@@ -31,10 +41,25 @@ void read_file(FILE *build_pointer, char *filename) {
     buf = strtok(line, " ");
     // While there are words to get
     while (buf != NULL) {
+      // Make all words lowercase
+      tolower(buf);
       // If the word isn't big enough skip
       if (strlen(buf) >= rfs->min_word_length) {
+        // For every word saved already
+        for (int y = 0; y < wordcounter; y++) {
+          // If word is already added to line
+          if (!strcmp(ws.wordlist[y], buf)) {
+            buf = strtok(NULL, " ");
+            flag = 1;
+            break;
+          }
+        }
+        if (flag)
+          continue;
         // Add word to given text file
+        ws.wordlist[wordcounter] = strdup(buf);
         fprintf(build_pointer, "%s ", buf);
+        ws.wordcounter++;
       }
       buf = strtok(NULL, " ");
     }
